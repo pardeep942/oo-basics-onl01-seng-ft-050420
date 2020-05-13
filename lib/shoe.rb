8d1813cfafca4086209e18804845c993fcc03a73
PARDEEP KAUR <pkaur84@aol.com>
2020-05-13 06:36:18 +0000

    Automatically backed up by Learn

---
+++ lib/shoe.rb
@@ -0,0 +1,13 @@
+class shoe
+  attr_accessor :nike, :page_count, :genre
+  attr_reader :title
+ 
+  def initialize(title)
+    @title = title
+  end
+ 
+  def turn_page
+    puts "Flipping the page...wow, you read fast!"
+  end
+ 
+end
